844d3edce70fa96c79e8a396bd8e25d89e2006dc
Ryan Murphy <murjax@gmail.com>
2017-07-22 13:28:26 -0400

    Refactor dashboard show page

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,3 +1,11 @@
 class Dashboard < ApplicationRecord
   belongs_to :user
+
+  def home_address_for_maps
+    "#{self.user.home_address}+#{self.user.home_city}+#{self.user.home_state}"
+  end
+
+  def work_address_for_maps
+    "#{self.user.work_address}+#{self.user.work_city}+#{self.user.work_state}"
+  end
 end
